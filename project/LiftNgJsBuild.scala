8add0fb83b8222d89d0a7eb8c0a8f466223f7ee4
Joe Barnes <barnesjd@gmail.com>
2014-05-09 09:45:33 -0500

    Stubbed out the fetch task

--- project/LiftNgJsBuild.scala
+++ project/LiftNgJsBuild.scala
@@ -5,9 +5,17 @@ object LiftNgJsBuild extends Build {
   val liftVersion = SettingKey[String]("liftVersion", "Full version number of the Lift Web Framework")
   val liftEdition = SettingKey[String]("liftEdition", "Lift Edition (short version number to append to artifact name)")
 
+  val fetchKey = TaskKey[Seq[File]]("fetch", "Fetch the angular modules for the configured version from https://code.angularjs.org")
+
+  val fetch = fetchKey <<= (version, streams) map { (v, s) =>
+    val ver = if(v.endsWith("-SNAPSHOT")) v.substring(0, v.length-9) else v
+    s.log.info(s"Fetching https://code.angularjs.org/$ver")
+    Seq()
+  }
+
   lazy val project = Project(
     id = "lift-ng-js", 
     base = file("."),
-    settings = Project.defaultSettings
+    settings = Project.defaultSettings ++ Seq(fetch)
   )
 }
