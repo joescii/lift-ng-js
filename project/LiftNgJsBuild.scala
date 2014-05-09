e8fd83f140c7e9e024028ab52e9182d4f32c5ac8
Joe Barnes <barnesjd@gmail.com>
2014-05-09 10:28:54 -0500

    fetch now grabs the zip file

--- project/LiftNgJsBuild.scala
+++ project/LiftNgJsBuild.scala
@@ -1,23 +1,51 @@
+import dispatch.Future
 import sbt._
 import sbt.Keys._
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object LiftNgJsBuild extends Build {
   val liftVersion = SettingKey[String]("liftVersion", "Full version number of the Lift Web Framework")
   val liftEdition = SettingKey[String]("liftEdition", "Lift Edition (short version number to append to artifact name)")
+  val baseUrl = SettingKey[String]("baseUrl", "The base URL for fetching angular code")
+  val zipUrl  = SettingKey[String]("zipUrl", "The URL for the zip file containing the angular code")
 
-  val fetchKey = TaskKey[Seq[File]]("fetch", "Fetch the angular modules for the configured version from https://code.angularjs.org")
+  val fetch = TaskKey[Seq[File]]("fetch", "Fetch the angular modules for the configured version from https://code.angularjs.org")
 
-  val fetch = fetchKey <<= (version, streams) map { (v, s) =>
+  val defaultZipUrl = zipUrl <<= (version, baseUrl) { (v, url) =>
     val ver = if(v.endsWith("-SNAPSHOT")) v.substring(0, v.length-9) else v
-    s.log.info(s"Fetching https://code.angularjs.org/$ver")
-    Seq()
+    val base = if(url.endsWith("/")) url else url + "/"
+    s"$base$ver/angular-$ver.zip"
   }
 
-  val requireFetch = resourceGenerators in Compile <+= fetchKey
+  val defaultFetch = fetch <<= (zipUrl, streams) map { (zip, s) =>
+    s.log.info(s"Fetching $zip")
+    val f = unpackZip(fetchZip(zip))
+    Await.result(f, 60.seconds)
+  }
+
+  private def fetchZip(zipUrl:String): Future[Array[Byte]] = {
+    import dispatch._
+    Http(url(zipUrl) OK as.Bytes)
+  }
+
+  private def unpackZip(bytes:Future[Array[Byte]]):Future[Seq[File]] = {
+    bytes.map( b =>
+      Seq()
+    )
+  }
+
+  val requireFetch = resourceGenerators in Compile <+= fetch
 
   lazy val project = Project(
     id = "lift-ng-js", 
     base = file("."),
-    settings = Project.defaultSettings ++ Seq(fetch, requireFetch)
+    settings = Project.defaultSettings ++ Seq(
+      defaultFetch,
+      requireFetch,
+      baseUrl := "https://code.angularjs.org/",
+      defaultZipUrl
+    )
   )
 }
---
+++ project/build.sbt
@@ -0,0 +1,4 @@
+libraryDependencies ++= Seq(
+  "net.databinder.dispatch" %% "dispatch-core" % "0.11.0" % "compile" // http://dispatch.databinder.net/Dispatch.html
+)
+
