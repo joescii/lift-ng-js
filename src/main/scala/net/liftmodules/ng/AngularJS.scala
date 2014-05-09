80f55b4fe71ea61c169dddbc5a0b9be1de283afd
Joe Barnes <barnesjd@gmail.com>
2014-05-09 09:31:41 -0500

    Added AngularJS.init(Seq[String]) stub

--- src/main/scala/net/liftmodules/ng/AngularJS.scala
+++ src/main/scala/net/liftmodules/ng/AngularJS.scala
@@ -4,9 +4,13 @@ import net.liftweb.http.DispatchSnippet
 import scala.xml.NodeSeq
 
 object AngularJS extends DispatchSnippet {
+  private var modules:Seq[String] = Seq()
+
+  def init(modules:Seq[String]) = this.modules = modules
+
   override def dispatch = {
     case _ => { _ => render }
   }
-  
+
   def render: NodeSeq = NodeSeq.Empty
 }
