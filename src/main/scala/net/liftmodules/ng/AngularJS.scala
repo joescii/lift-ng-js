d6606428763bb75cb1e274f459f0fcf277ea0214
Joe Barnes <barnesjd@gmail.com>
2014-05-09 09:51:00 -0500

    Adding our AngularJS snippet to the LiftRules snippetDispatch

--- src/main/scala/net/liftmodules/ng/AngularJS.scala
+++ src/main/scala/net/liftmodules/ng/AngularJS.scala
@@ -1,12 +1,18 @@
 package net.liftmodules.ng
 
-import net.liftweb.http.DispatchSnippet
+import net.liftweb.http.{LiftRules, DispatchSnippet}
 import scala.xml.NodeSeq
 
 object AngularJS extends DispatchSnippet {
   private var modules:Seq[String] = Seq()
 
-  def init(modules:Seq[String]) = this.modules = modules
+  def init(modules:Seq[String]) = {
+    this.modules = modules
+
+    LiftRules.snippetDispatch.append {
+      case "AngularJS" => this
+    }
+  }
 
   override def dispatch = {
     case _ => { _ => render }
